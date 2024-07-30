784616759360851a5405fbc4a9c93684015bb684
LemoZun <marrzun@gmail.com>
2024-07-30 20:24:56 +0900

    feat : 동적 인벤토리, 요세푸스 순열

---
+++ DynamicInventory.cs
@@ -0,0 +1,173 @@
+﻿using System.Security.Cryptography;
+
+namespace HW
+{
+    internal class DynamicInventory
+    {
+        public class Item
+        {
+            protected string name;
+            public string GetName
+            {
+                get { return name; }
+
+            }
+            public class Inventory
+            {
+                public List<Item> items = new List<Item>(9);
+
+            }
+            public class Potion : Item
+            {
+                // int recoveryAmount;
+
+                public Potion(string name)
+                {
+                    // this.recoveryAmount = recoveryAmount;
+                    this.name = name;
+                }
+
+
+            }
+            public class Weapon : Item
+            {
+                // int attackPoint;
+                public Weapon(string name)
+                {
+                    // this.attackPoint = attackPoint;
+                    this.name = name;
+                }
+
+            }
+            public class Armor : Item
+            {
+                // int defensePoint;
+                public Armor(string name)
+                {
+                    // this.defensePoint = defensePoint;
+                    this.name = name;
+                }
+
+            }
+
+            public class Accessory : Item
+            {
+                // string ability;
+
+                public Accessory(string name)
+                {
+                    // this.ability = ability;
+                    this.name = name;
+                }
+            }
+            public class Food : Item
+            {
+                // int fullness;
+                public Food(string name)
+                {
+                    // this.fullness = fullness;
+                    this.name = name;
+                }
+            }
+
+            static void Main1(string[] args)
+            {
+
+                Inventory inventory = new Inventory();
+                int num;
+                bool toDetermine;
+                //inventory.items[8] = new Potion(10, "회복포션");
+                // inventory.items.Add(new Potion(10, "회복포션")); // 아이템 넣기
+                //Potion potion = new Potion();
+                //potion.name = "a";
+                while (true)
+                {
+                    int count = 1;
+                    Console.WriteLine("현재 인벤토리");
+                    foreach (Item item in inventory.items)
+                    {
+                        
+                        Console.WriteLine($"{count} : {item.name}");
+                        count++;
+                    }
+
+                    Console.WriteLine("아이템을 추가하려면 숫자0을, 아이템을 삭제하려면 아이템의 번호를 눌러주세요.");
+                    toDetermine = int.TryParse(Console.ReadLine(), out num);
+
+                    if (inventory.items.Count >= 9 && num==0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("더이상 추가 불가"); //추가 불가능하다도 해야함
+                        Console.ResetColor();
+                        continue;
+                    }
+
+                    if (num == 0)
+                    {
+                        AddItem(inventory, CreateItem());
+                    }
+                    else if (inventory.items[num-1] == null) // null조차 아니라 공간 자체가 할당 안되서 안됨
+                    {
+                        Console.WriteLine("그 공간은 이미 비었습니다."); 
+                        continue;
+                    }
+                    // { num == 0} ? AddItem(inventory, CreateItem()); //값을 반환할 변수가 있어야함
+
+                    if (toDetermine == false || (num > 9 && num < 0)) //아이템이 없는 경우는 나중에 뭔가 조건이 이상함
+                    {
+                        Console.WriteLine("잘못된 입력");
+                        continue;
+                    }                    
+                    else if (num > 0 && num < 9)
+                    {
+                        Console.WriteLine($"{num}번째 아이템 {inventory.items[num - 1].name} 을 버렸습니다.");
+                        RemoveItem(inventory, num);
+                    }
+                    
+                    // object result = inventory.items[num] ?? AddItem(inventory, CreateItem()) ; 
+                    // AddItem(inventory, CreateItem());                    
+                }
+            }
+
+            static public void AddItem(Inventory _inventory, Item _item)
+            {
+                _inventory.items.Add(_item);
+            }
+            static public void RemoveItem(Inventory _inventory, int _num)
+            {
+                _inventory.items.RemoveAt(_num-1);
+            }
+            static public Item CreateItem()
+            {
+                Random rand = new Random();
+                int randNum = rand.Next(0, 5);
+                // 5개의 아이템 중 랜덤으로 만들기
+                switch (randNum)
+                {
+                    case 0:
+                        return new Potion("포션");
+                    case 1:
+                        return new Weapon("무기");
+                    case 2:
+                        return new Armor("갑옷");
+                    case 3:
+                        return new Accessory("장신구");
+                    case 4:
+                        return new Food("음식");
+                    default:
+                        Console.WriteLine("입력 에러");
+                        return new Item(); // 이러면 안되는데 Item을 return 안하고 강제 종료시킬 순 없나?
+                        
+                }
+            }
+
+
+
+
+        }
+
+    }
+
+
+}
+
---
+++ JosephusPermutation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    internal class JosephusPermutation
+    {
+        static void Main()
+        {
+            /*
+             * 1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다. 
+             * 이제 순서대로 K번째 사람을 제거한다.
+             * 한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다. 
+             * 이 과정은 N명의 사람이 모두 제거될 때까지 계속된다. 
+             * 원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열이라고 한다.
+             * 예를 들어 (7, 3)-요세푸스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+             */
+
+
+            LinkedList<int> josephus = new LinkedList<int>();
+            int n;
+            int k; // k의 배수만큼 뛰어넘어서 제거
+
+            Console.WriteLine("요세푸스 순열");
+            Console.WriteLine("사람이 총 몇명인지 입력해주세요.");
+            bool toDetermine1 = int.TryParse(Console.ReadLine(), out n);
+            Console.WriteLine("몇번째 사람을 제거할 지 입력해주세요.");
+            bool toDetermine2 = int.TryParse(Console.ReadLine(),out k);
+            Console.Clear();
+
+            for (int i = 1; i <=n; i++)
+            {
+                josephus.AddLast(i);
+            }
+
+            LinkedListNode<int> node1; // = josephus.First;
+
+            /* ➀-➁-➂-➃-➄-➅-➆
+             * 3,6,2,7,5,1,4 순으로 빠져야함
+             * k의 배수만큼 건너뜀
+             * 원형 연결리스트 or 다른방법
+             * ➂-➃-➄-➅-➆-➀-➁ 제거하지 않은 수는 맨 뒤에 연결하고 반복 돌리면 .
+             * ➃-➄-➅-➆-➀-➁ -> 3 제거
+             * ➅-➆-➀-➁-➃-➄
+             * ➆-➀-➁-➃-➄ -> 6 제거
+             * ➁-➃-➄-➆-➀
+             * ➃-➄-➆-➀ -> 2 제거
+             */
+
+            while (josephus.Count>0) // 남아있는게 없을때까지 뺌
+            {
+                for(int i = 1; i <=k; i++) // 매번 k만큼 반복을 돌려서
+                {
+                    // josephus.Remove(k); //이건 값을 찾아서 제거하는것, index 없음
+                    // 찾아서 지우려면 linkedlistnode를 써야함 
+                    node1 = josephus.First;
+                    
+                    if(i == k) // k까지 오면 빼고
+                    {
+                        // Console.Write($"{node1.Value}을(를) 제거합니다.");
+                        josephus.Remove(node1);
+                        Console.WriteLine($"{node1.Value}");
+                    }
+                    else // 아니면 맨 뒤에 붙임
+                    {
+                        // Console.Write($"{node1.Value}을(를) 맨 뒤로 보냅니다.");
+                        josephus.RemoveFirst();
+                        josephus.AddLast(node1); 
+                    }
+                }
+                
+            }
+            /*1.  4 1 6 5 7 3 2 k+1만큼 뛰어넘고 있음 일단 뛰어넘는건 구현
+             *  for 반복문 i를 1부터 바꿈
+             */
+
+
+
+        }
+    }
+}
